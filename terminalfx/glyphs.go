28f5033654e1eae34ae7c92e43b90f8bf5d21d7f
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-06-18 12:27:31 -0400

    better glyph rendering

--- terminalfx/glyphs.go
+++ terminalfx/glyphs.go
@@ -19,7 +19,7 @@ const (
 )
 
 // Stop signs
-func EuropeanStopSign(sentence string) string { return fmt.Sprintf("⛔ %s%s", reset, sentence) }
+func EuropeanStopSign(sentence string) string { return fmt.Sprintf("⛔️ %s%s", reset, sentence) }
 
 func AmericanStopSign(sentence string) string { return fmt.Sprintf("🛑 %s%s", reset, sentence) }
 
@@ -28,17 +28,17 @@ func BombSign(sentence string, coloured bool) string {
 	return fmt.Sprintf("💥 %s%s", reset, sentence)
 }
 
-func SkullBonesSign(sentence string) string { return fmt.Sprintf("☠ %s%s", reset, sentence) }
+func SkullBonesSign(sentence string) string { return fmt.Sprintf("☠️ %s%s", reset, sentence) }
 
 // Accepted/denied
-func EnabledSign(sentence string) string { return fmt.Sprintf("✅ %s%s", reset, sentence) }
-func ErrorSign(sentence string) string   { return fmt.Sprintf("❌ %s%s", reset, sentence) }
+func EnabledSign(sentence string) string { return fmt.Sprintf("✅️ %s%s", reset, sentence) }
+func ErrorSign(sentence string) string   { return fmt.Sprintf("❌️ %s%s", reset, sentence) }
 
 // Status / utility// InProgressChar returns a single-glyph indicator for "task in progress".
 func GreenGoSign(sentence string) string { return fmt.Sprintf("🟢 %s%s", reset, sentence) }
 
-func InProgressSign(sentence string) string { return fmt.Sprintf("⏳ %s%s", reset, sentence) } // U+23F3 HOURGLASS NOT DONE
-func WarningSign(sentence string) string    { return fmt.Sprintf("⚠ %s%s", reset, sentence) } // U+26A0 WARNING SIGN
+func InProgressSign(sentence string) string { return fmt.Sprintf("⏳️ %s%s", reset, sentence) } // U+23F3 HOURGLASS NOT DONE
+func WarningSign(sentence string) string    { return fmt.Sprintf("⚠️ %s%s", reset, sentence) } // U+26A0 WARNING SIGN
 func InfoSign(sentence string) string       { return fmt.Sprintf("🛈 %s%s", reset, sentence) } // circled info (U+1F6C8)
 func NoteSign(sentence string) string       { return fmt.Sprintf("💬 %s%s", reset, sentence) } // speech bubble
 func ScrollSign(sentence string) string     { return fmt.Sprintf("📜 %s%s", reset, sentence) } // scroll/document
